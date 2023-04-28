858053644512b0fef82bf5c025170ab45288fae1
TechDanny <kokidanny15@gmail.com>
2023-04-28 11:43:51 -0700

    printf c files

--- main.h
+++ main.h
@@ -5,11 +5,18 @@
 #include <stdarg.h>
 #include <stdlib.h>
 
+typedef struct format_specifier
+{
+	char *specifier;
+	int (*print_n)(va_list list);
+}format_t;
+
 int my_putchar(char a);
-int my_putstr(char *str);
+int my_putstr(va_list list);
 int _printf(const char *format, ...);
 void print_s(char *s);
-void print_char(char c);
+void print_char(va_list list);
 void my_atoi(int digit, char *str);
+int my_print_percent(__attribute__((unused)) va_list list);
 
 #endif
---
+++ my_print_percent.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * my_print_percent - prints the modulus sign
+ * @list:list of arguments
+ *
+ * Return:returns 0
+ */
+
+int my_print_percent(__attribute__((unused)) va_list list)
+{
+	my_putchar(%);
+	return (1);
+}
