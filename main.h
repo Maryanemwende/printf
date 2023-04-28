38027361c1af2f854570c4f99424df74549e22ca
TechDanny <kokidanny15@gmail.com>
2023-04-28 03:39:38 -0700

    printf c files

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+int my_putchar(char a);
+int my_putstr(char *str);
+int _printf(const char *format, ...);
+
+
+#endif
---
+++ my_putstr.c
@@ -0,0 +1,29 @@
+include "main.h"
+
+/**
+ * my_putstr - prints a string
+ * @str:string to be printed
+ *
+ * Return:a value
+ */
+
+int my_putstr(char *str)
+{
+	int elem = 0;
+	int count = 0;
+
+	if (str)
+	{
+		while (str[elem] != '\0')
+		{
+			my_putchar(str[elem]);
+			count += 1;
+			elem++;
+		}
+	}
+	else if (str == NULL)
+		return (NULL);
+
+	return (count);
+}
+
